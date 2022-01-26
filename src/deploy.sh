cff1966d937ae4827f49e911683ebf8a36800980
张进 <jinzhanga@trip.com>
2022-01-26 15:38:14 +0800

    prune

--- src/deploy.sh
+++ src/deploy.sh
@@ -2,5 +2,4 @@
 
 docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:"eshop-$1"
 docker-compose --env-file ./.env.arm -f docker-compose-ARM.yml -f docker-compose-ARM.override.yml up -d --force-recreat $1
-
-
+docker image prune -f
