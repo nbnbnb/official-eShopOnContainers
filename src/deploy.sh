a9dc03e3ca526862fa78451f5e24e90afdb05b44
JJZhang <dashan1010zj@163.com>
2023-02-12 22:39:33 +0800

    clean

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# haha
-
 docker pull registry.cn-hangzhou.aliyuncs.com/zhangjj/demo:"eshop-$1"
 docker-compose --env-file ./.env.arm -f docker-compose-ARM.yml -f docker-compose-ARM.override.yml up -d $1
 docker image prune -f
